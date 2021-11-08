c5532d3c2a61996b238b4913ebc6d7174cf5cd45
CarlosMontesdeoca <mateo.game@hotmail.com>
2021-11-08 18:17:41 -0500

    uso de material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import faker from "faker";
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
 
 function App() {
   const [num, setNum] = useState(1);
@@ -63,51 +72,79 @@ function App() {
   };
 
   return (
-    <div>
-      <h1> Ejercicio Simpsons</h1>
+    <div className="container">
+      <div className="box-container">
+       <div className="title"><h1> Ejercicio Simpsons</h1></div>
       <br />
-      <input
-        type="text"
-        name="edit"
-        id="edit"
-        onChange={(e) => setAux(e.target.value)}
-      />
-      <button onClick={() => handleNext()}>
+      <Button onClick={() => handleNext()} variant="contained" disableElevation>
         {" "}
         Next{" "}
-      </button>
+      </Button>
       <div className="random">
         {simpson.map((simp, index) => (
           <div key={simp.quote} className="card-content" id="1">
-            <div>
-              <img src={simp.image} className="card" alt="photo" />
-            </div>
-            <div>
-              <h3> {simp.character} </h3>
-              <p> {simp.quote} </p>
-              <button 
-                onClick={() => handleAddFavorites(simp)}
-              >
-                Favoritos
-              </button>
-            </div>
+            <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    {simp.character}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary" component="div">
+                    {simp.quote} 
+                  </Typography>
+                </CardContent>
+                <Button variant="contained" onClick={() => handleAddFavorites(simp)}>
+                  Add
+                </Button>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={simp.image}
+                alt="Live from space album cover"
+              />
+            </Card>
           </div>
         ))}
       </div>
       <h2>Favoritos</h2>
+      <TextField 
+        id="outlined-basic" 
+        label="New Text" 
+        variant="filled"
+        onChange={(e) => setAux(e.target.value)} 
+      />
       {favorites.map((favorit, index) => (
         <div key={index} className="card-content random" id="1">
-          <div>
-            <img src={favorit.image} className="card" alt="photo" />
-          </div>
-          <div>
-            <h3> {favorit.character} </h3>
-            <p> {favorit.quote} </p>
-            <button onClick={() => handleEdit(favorit.id, aux)}>Edit</button>
-            <button onClick={() => handleDelete(favorit.id)}>Delet</button>
-          </div>
+          <Card sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography component="div" variant="h5">
+                    {favorit.character}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary" component="div">
+                    {favorit.quote} 
+                  </Typography>
+                </CardContent>
+                <Stack direction="row" spacing={2}>
+                  <Button variant="outlined" onClick={() => handleDelete(favorit.id)}>
+                    Delete
+                  </Button>
+                  <Button variant="contained" onClick={() => handleEdit(favorit.id, aux)}>
+                    Edit
+                  </Button>
+                </Stack>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={favorit.image}
+                alt="Live from space album cover"
+              />
+            </Card>
         </div>
       ))}
+      </div>
     </div>
   );
 }
