92df4ad221e591b6000bec3cd65f49c6d9511a8b
CarlosMontesdeoca <mateo.game@hotmail.com>
2021-11-08 13:23:50 -0500

    api funcional

--- src/App.js
+++ src/App.js
@@ -1,91 +1,113 @@
-
-import './App.css';
-import {useEffect, useState} from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
+import faker from "faker";
 
 function App() {
-
-  const [num, setNum] = useState("1");
-  const [favorite, setFavorite] = useState([]);
-
-  const [image, setImage] = useState("");
-  const [name, setName] = useState("");
-  const [text, setText] = useState("");
-
-  const [callApi, setCallApi] = useState(false);
+  const [num, setNum] = useState(1);
+  const [aux, setAux] = useState("");
+  const [favorites, setFavorites] = useState([]);
   const [simpson, setSimpson] = useState([]);
-    
-    useEffect( () => {
-      const getSimpson = async() => {
-        const response = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${num}`);
-        const simpsonsArray = await response.json();
-        console.log("simpsonsArray", simpsonsArray[0]);
-        console.log("simpson", simpson);
-        setSimpson(simpsonsArray);
-        setCallApi(false);
-      };
-      callApi && getSimpson();
-    }, [callApi]);
 
-    useEffect( () => {
+  const getSimpson = async () => {
+    const response = await fetch(
+      `https://thesimpsonsquoteapi.glitch.me/quotes`
+    );
+    const simpsonsArray = await response.json();
+    //console.log("simpson", simpson);
+    setSimpson(simpsonsArray);
+  };
+  useEffect(() => {
+    getSimpson();
+  }, []);
 
-    })
+  const handleNext = () => {
+    getSimpson();
+    //console.log("simpson: ", simpson[0]);
+  };
 
-  const handleNext = ( number) => {
-    //var card = document.getElementById("1");
-    console.log("num", number);
-    setNum( number)
-    setCallApi(true)
-  }
+  const handleAddFavorites = (favorit) => {
+    const newSimpson = {
+      image: simpson[0].image,
+      character: simpson[0].character,
+      quote: simpson[0].quote,
+      id: num
+    }
+    console.log("nuevo", newSimpson);
+    setFavorites([...favorites, newSimpson]);
+    setNum(num + 1);
+    getSimpson();
+  };
 
-  const handleAddFavorites = ( imagesimp, namesimp, textsimp) => {
-    setImage(imagesimp);
-    setName(namesimp);
-    setText(textsimp);
+  const handleDelete = (index) => {
+    const newSimpsons = favorites.filter((favorite) => favorite.id !== index);
+    setFavorites(newSimpsons);
+    //console.log("elemento", index);
+    //console.log("favoritos", favorites);
+  };
 
-    console.log("image", image);
-    console.log("name", name);
-    console.log("text", text);
-  }
+  const handleEdit = (index, newText) => {
+    const newSimpsons = favorites.map((favorite) => {
+
+      if (favorite.id === index && newText != "") {
+        console.log("nuevo", newText);
+        
+        return {
+          ...favorite,
+          quote: newText,
+        }
+      }
+      return favorite
+    });
+
+    setFavorites(newSimpsons);
+  };
 
   return (
     <div>
-      <h1>  Ejercicio Simpsons</h1>
-      <br/>
-      <input type="number" name="num" id="num"/>
-      <button onClick={() => handleNext (document.getElementById("num"))} > Next </button>
-        <div className="random">
-        
-          {
-            simpson.map( (simp) => (
-              <div className="card-content" id="1">
-                <div>
-                  <img src= {simpson[0].image} className="card" />
-                  </div>
-                <div>
-                  <h3> {simpson[0].character} </h3>
-                  <p> {simpson[0].quote} </p>  
-                  <button onClick={ () => handleAddFavorites (simpson.image, simpson.character, simpson.quote)} >Favoritos</button>
-                </div>
-              </div>
-              
-            ))
-          }
-        </div>
-      <h2>Favoritos</h2>
-      {
-        favorite.map( (favorit) => (
-          <div className="card-content" id="1">
+      <h1> Ejercicio Simpsons</h1>
+      <br />
+      <input
+        type="text"
+        name="edit"
+        id="edit"
+        onChange={(e) => setAux(e.target.value)}
+      />
+      <button onClick={() => handleNext()}>
+        {" "}
+        Next{" "}
+      </button>
+      <div className="random">
+        {simpson.map((simp, index) => (
+          <div key={simp.quote} className="card-content" id="1">
             <div>
-              <img src= {simpson[0].image} className="card" />
+              <img src={simp.image} className="card" alt="photo" />
             </div>
             <div>
-              <h3> {simpson[0].character} </h3>
-              <p> {simpson[0].quote} </p>  
-              <button onClick={handleAddFavorites} >Favoritos</button>
+              <h3> {simp.character} </h3>
+              <p> {simp.quote} </p>
+              <button 
+                onClick={() => handleAddFavorites(simp)}
+              >
+                Favoritos
+              </button>
             </div>
           </div>
-        ))
-      }
+        ))}
+      </div>
+      <h2>Favoritos</h2>
+      {favorites.map((favorit, index) => (
+        <div key={index} className="card-content random" id="1">
+          <div>
+            <img src={favorit.image} className="card" alt="photo" />
+          </div>
+          <div>
+            <h3> {favorit.character} </h3>
+            <p> {favorit.quote} </p>
+            <button onClick={() => handleEdit(favorit.id, aux)}>Edit</button>
+            <button onClick={() => handleDelete(favorit.id)}>Delet</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
